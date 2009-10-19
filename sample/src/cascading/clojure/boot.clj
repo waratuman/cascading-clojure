cee577e477ff1f8e760278b5e9880d251404d0bd
bradford cross <bradford@bradford-laptop.(none)>
2009-10-19 08:51:13 -0700

    fixing the sample code.

--- sample/src/cascading/clojure/boot.clj
+++ sample/src/cascading/clojure/boot.clj
@@ -1,10 +1,8 @@
  (ns cascading.clojure.boot
-  (:require [org.parsimonygroup.cascading :as c])
+  (:use org.parsimonygroup.cascading)
   (:require [org.parsimonygroup.makemain-utils :as m])
   (:gen-class))
 	
 (defn -main [& args]
   (let [opts (assoc (m/parse-args args) :main-class (class -main))]
-      (c/cascading opts)))
-
-
+      (execute (workflow opts))))
