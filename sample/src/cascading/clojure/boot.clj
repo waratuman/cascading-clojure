6e846ca68b3c95587d639a46127fe1826fa0421b
bradford cross <bradford@bradford-laptop.(none)>
2009-10-20 19:48:24 -0700

    fixing a few issues in the samples.

--- sample/src/cascading/clojure/boot.clj
+++ sample/src/cascading/clojure/boot.clj
@@ -1,6 +1,6 @@
  (ns cascading.clojure.boot
-  (:use org.parsimonygroup.cascading)
-  (:require [org.parsimonygroup.makemain-utils :as m])
+  (:use cascading.clojure.cascading)
+  (:require [cascading.clojure.makemain-utils :as m])
   (:gen-class))
 	
 (defn -main [& args]
