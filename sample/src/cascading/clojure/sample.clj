ef4875fae8b027101ea93587c70c176915a73d12
bradford cross <bradford@bradford-laptop.(none)>
2009-10-20 19:48:24 -0700

    fixing a few issues in the samples.

--- sample/src/cascading/clojure/sample.clj
+++ sample/src/cascading/clojure/sample.clj
@@ -16,22 +16,22 @@
   (not (= "dummycontent" name)))
 
 (defn test-with-fields []
-  {:operations {:each {:using split-line :reader identity :writer str :outputFields ["name" "id" "content"]}
+  {:each {:using split-line :reader identity :writer str :outputFields ["name" "id" "content"]}
 		:each {:using identity-each :reader identity :writer str :inputFields ["name" "id"] :outputFields ["name" "id"]}
-		:filter {:using filter-dummycontent-name :reader identity :writer str :inputFields ["name" "id"] :outputFields ["name" "id"]}}})
+		:filter {:using filter-dummycontent-name :reader identity :writer str :inputFields ["name" "id"] :outputFields ["name" "id"]}})
 	
 (defn test-with-fields1 []
-  {:operations {:each {:using split-line :reader identity :writer str :outputFields ["name1" "id1" "content1"]}
+  {:each {:using split-line :reader identity :writer str :outputFields ["name1" "id1" "content1"]}
 		:each {:using identity-each :reader identity :writer str :inputFields ["name1" "id1"] :outputFields ["name1" "id1"]}
-		:filter {:using filter-dummycontent-name :reader identity :writer str :inputFields ["name1" "id1"] :outputFields ["name1" "id1"]}}})
+		:filter {:using filter-dummycontent-name :reader identity :writer str :inputFields ["name1" "id1"] :outputFields ["name1" "id1"]}})
 
 (defn append-str [acc nxt]
   (let [seen (first acc)]
     [(str seen nxt)]))
 				
 (defn groupby-with-fields []
-  {:operations {:groupBy {:using second-of-line :reader identity :writer str :outputFields ["key" "second"]}
-		:everygroup {:using append-str :reader identity :init (fn [] [""]) :writer str :inputFields ["second"] :outputFields ["combined"]}}})
+  {:groupBy {:using second-of-line :reader identity :writer str :outputFields ["key" "second"]}
+		:everygroup {:using append-str :reader identity :init (fn [] [""]) :writer str :inputFields ["second"] :outputFields ["combined"]}})
 
 
 (defn sample-join []
