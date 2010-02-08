64a5e4a2b082bc56918d26f8fae1c37c86408df2
Mark McGranaghan <mmcgrana@gmail.com>
2010-02-07 23:17:37 -0500

    Tidy up api.clj implementation, pull in c/first from nm-fn-api.

--- src/clj/cascading/clojure/api.clj
+++ src/clj/cascading/clojure/api.clj
@@ -1,12 +1,12 @@
 (ns cascading.clojure.api
-  (:refer-clojure :exclude (count filter mapcat map))
+  (:refer-clojure :exclude (count first filter mapcat map))
   (:use [clojure.contrib.seq-utils :only [find-first indexed]])
   (:import (cascading.tuple Fields)
            (cascading.scheme TextLine)
            (cascading.flow Flow FlowConnector)
            (cascading.operation Identity)
            (cascading.operation.regex RegexGenerator RegexFilter)
-           (cascading.operation.aggregator Count)
+           (cascading.operation.aggregator First Count)
            (cascading.pipe Pipe Each Every GroupBy CoGroup)
            (cascading.pipe.cogroup InnerJoin)
            (cascading.scheme Scheme)
@@ -34,7 +34,7 @@
     (coll? v-or-coll)
       (into-array Object
         (concat
-          (ns-fn-name-pair (first v-or-coll))
+          (ns-fn-name-pair (clojure.core/first v-or-coll))
           (next v-or-coll)))
     :else
       (throw (IllegalArgumentException. (str v-or-coll)))))
@@ -57,32 +57,51 @@
     (and (sequential? obj) (every? string? obj))))
 
 (defn- idx-of-first [coll pred]
-  (first (find-first #(pred (last %)) (indexed coll))))
+  (clojure.core/first (find-first #(pred (last %)) (indexed coll))))
 
-(defn- parse-func [obj]
-  "obj =>
+; in-fields: subset of fields from incoming pipe that are passed to function
+;   defaults to all
+; func-fields: fields declared to be returned by the function
+;   must be given in meta data or as [override-fields #'func-var]
+;   no default, error if missing
+; out-fields: subset of (union in-fields func-fields) that flow out of the pipe
+;   defaults to func-fields
+
+(defn- parse-func [func-obj]
+  "func-obj =>
    #'func
    [#'func]
-   [overridefields #'func]
-   [#'func params...]
-   [overridefields #'func params...]"
-  (let [obj         (collectify obj)
-        i           (idx-of-first obj var?)
-        spec        (fn-spec (drop i obj))
-        funcvar     (nth obj i)
-        func-fields (fields (if (> i 0) (first obj) ((meta funcvar) :fields)))]
-    [func-fields spec]))
+   [override-fields #'func]
+   [#'func & params]
+   [override-fields #'func & params]"
+  (let [func-obj    (collectify func-obj)
+        i           (idx-of-first func-obj var?)]
+    (assert (<= i 1))
+    (let [spec        (fn-spec (drop i func-obj))
+          func-var    (nth func-obj i)
+          func-fields (or (and (= i 1) (clojure.core/first func-obj))
+                          ((meta func-var) :fields))]
+      (when-not func-fields
+        (throw (Exception. (str "no fields assocaiated with " func-obj))))
+      [(fields func-fields) spec])))
 
 (defn- parse-args
-  ([arr]
-   (parse-args arr Fields/RESULTS))
-  ([arr defaultout]
-   (let [i                  (idx-of-first arr #(not (fields-obj? %)))
-         infields           (if (> i 0) (fields (first arr)) Fields/ALL)
-         [func-fields spec] (parse-func (nth arr i))
-         outfields          (if (< i (dec (clojure.core/count arr)))
-                              (fields (last arr)) defaultout)]
-     [infields func-fields spec outfields])))
+  "arr =>
+  [func-obj]
+  [in-fields func-obj]
+  [in-fields func-obj out-fields]
+  [func-obj out-fields]"
+  [arr]
+  (let [i (idx-of-first arr (complement fields-obj?))]
+    (assert (<= i 1))
+    (let [in-fields          (if (= i 1)
+                               (fields (clojure.core/first arr))
+                               Fields/ALL)
+          [func-fields spec] (parse-func (nth arr i))
+          out-fields         (if (< i (dec (clojure.core/count arr)))
+                               (fields (last arr))
+                               Fields/RESULTS)]
+    [in-fields func-fields spec out-fields])))
 
 (defn- uuid []
   (str (UUID/randomUUID)))
@@ -118,6 +137,9 @@
 
 (defn group-by [#^Pipe previous group-fields]
   (GroupBy. previous (fields group-fields)))
+
+(defn first [#^Pipe previous]
+  (Every. previous (First.)))
 
 (defn count [#^Pipe previous #^String count-fields]
   (Every. previous
@@ -160,16 +182,15 @@
        (.setProperty props k v))
      (.setProperty props "mapred.used.genericoptionsparser" "true")
      (let [flow-connector (FlowConnector. props)]
-       (.connect flow-connector source-map sink pipe)))))
+       (try
+         (.connect flow-connector source-map sink pipe)
+         (catch cascading.flow.PlannerException e
+           (.writeDOT e "exception.dot")
+           (throw (RuntimeException.
+             "see exception.dot to visualize your broken plan." e))))))))
 
 (defn write-dot [#^Flow flow #^String path]
   (.writeDOT flow path))
 
 (defn exec [#^Flow flow]
-  (try
-    (doto flow .start .complete)
-    (catch cascading.flow.PlannerException e
-      (.writeDOT e "exception.dot")
-      (throw (RuntimeException.
-        "see exception.dot to visualize your broken plan." e)))))
-
+  (doto flow .start .complete))
