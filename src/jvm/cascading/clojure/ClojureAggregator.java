76f5629b68c289a44bb400319126d322c172ea36
Mark McGranaghan <mmcgrana@gmail.com>
2010-02-07 23:16:44 -0500

    Less indirection in ClojureAggregator.

--- src/jvm/cascading/clojure/ClojureAggregator.java
+++ src/jvm/cascading/clojure/ClojureAggregator.java
@@ -12,11 +12,10 @@ import cascading.tuple.Fields;
 import clojure.lang.IFn;
 import clojure.lang.RT;
 import clojure.lang.ISeq;
-import clojure.lang.Box;
 import java.util.Collection;
 
-public class ClojureAggregator extends BaseOperation<Box>
-                               implements Aggregator<Box> {
+public class ClojureAggregator extends BaseOperation<Object>
+                               implements Aggregator<Object> {
   private Object[] start_fn_spec;
   private IFn      start_fn;
   private Object[] aggregate_fn_spec;
@@ -32,34 +31,33 @@ public class ClojureAggregator extends BaseOperation<Box>
     this.complete_fn_spec =   complete_fn_spec;
   }
   
-  public void prepare(FlowProcess flow_process, OperationCall<Box> op_call) {
+  public void prepare(FlowProcess flow_process, OperationCall<Object> op_call) {
     this.start_fn =     Util.bootFn(start_fn_spec);
     this.aggregate_fn = Util.bootFn(aggregate_fn_spec);
     this.complete_fn =  Util.bootFn(complete_fn_spec);
   }
 
-  public void start(FlowProcess flow_process, AggregatorCall<Box> ag_call) {
+  public void start(FlowProcess flow_process, AggregatorCall<Object> ag_call) {
     try {
-      ag_call.setContext(new Box(this.start_fn.invoke()));
+      ag_call.setContext(this.start_fn.invoke());
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
   }
 
-  public void aggregate(FlowProcess flow_process, AggregatorCall<Box> ag_call) {
+  public void aggregate(FlowProcess flow_process, AggregatorCall<Object> ag_call) {
     try {
       ISeq fn_args_seq = Util.coerceFromTuple(ag_call.getArguments().getTuple());
-      Box box = ag_call.getContext();
-      box.val = this.aggregate_fn.applyTo(RT.cons(box.val, fn_args_seq));
+      ag_call.setContext(
+        this.aggregate_fn.applyTo(RT.cons(ag_call.getContext(), fn_args_seq)));
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
   }
 
-  public void complete(FlowProcess flow_process, AggregatorCall<Box> ag_call) {
+  public void complete(FlowProcess flow_process, AggregatorCall<Object> ag_call) {
     try {
-      Box box = ag_call.getContext();
-      Collection coll = (Collection) this.complete_fn.invoke(box.val);
+      Collection coll = (Collection) this.complete_fn.invoke(ag_call.getContext());
       ag_call.getOutputCollector().add(Util.coerceToTuple(coll));
     } catch (Exception e) {
       throw new RuntimeException(e);
