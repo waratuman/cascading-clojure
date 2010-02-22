10113df8835b1792d4b1ceacb337592e41d6843b
jared strate <strate.jared@gmail.com>
2010-02-21 21:23:37 -0800

    update project file with swank

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
                  [cascading/cascading "1.0.17-SNAPSHOT"
                    :exclusions [javax.mail/mail janino/janino]]
                  [clj-json "0.2.0-SNAPSHOT"]]
-  :dev-dependencies [[lein-javac "0.0.2-SNAPSHOT"]]
+  :dev-dependencies [[lein-javac "0.0.2-SNAPSHOT"]
+                     [org.clojure/swank-clojure "1.0"]]
   :namespaces [cascading.clojure.api
                cascading.clojure.testing])
