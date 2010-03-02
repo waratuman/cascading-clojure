467cd48e586a4a51a722cd1583bb475d43e04f4b
waratuman <waratuman@gmail.com>
2010-03-02 22:22:36 +0000

    Finished the previous merge (forgot to remove old code files and ensure test ran).

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
                  [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
                  [cascading/cascading "1.0.17-SNAPSHOT"
                    :exclusions [javax.mail/mail janino/janino]]
+                 [clj-serializer "0.1.0-SNAPSHOT"]
                  [clj-json "0.2.0-SNAPSHOT"]]
   :dev-dependencies [[lein-javac "0.0.2-SNAPSHOT"]
                      [org.clojure/swank-clojure "1.0"]])
--- sample/src/cascading/clojure/boot.clj
+++
@@ -1,8 +0,0 @@
- (ns cascading.clojure.boot
-  (:use cascading.clojure.cascading)
-  (:require [cascading.clojure.makemain-utils :as m])
-  (:gen-class))
-	
-(defn -main [& args]
-  (let [opts (assoc (m/parse-args args) :main-class (class -main))]
-      (execute (workflow opts))))
