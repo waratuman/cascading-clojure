a76aacd4f7c35fdea27bccfbff930305502846a9
waratuman <waratuman@gmail.com>
2010-03-12 10:39:03 -0800

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject cascading-clojure "1.0.0-SNAPSHOT"
+  :source-path "src/clj"
+  :java-source-path "src/jvm"
+  :java-fork "true"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
+                 [cascading/cascading "1.0.17-SNAPSHOT"
+                   :exclusions [javax.mail/mail janino/janino]]
+                 [clj-serializer "0.1.0-SNAPSHOT"]
+                 [clj-json "0.2.0"]]
+  :dev-dependencies [[org.clojars.mmcgrana/lein-javac "0.1.0"]
+                     [swank-clojure "1.1.0"]]
+  :namespaces [cascading.clojure.api
+               cascading.clojure.testing])
---
+++ src/jvm/cascading/clojure/Util.java
@@ -0,0 +1,5 @@
+package cascading.clojure;
+
+public class Util {
+    
+}
